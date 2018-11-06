26e7503e4f736f3ee9087c5e23a06575885c1588
mplakalovi1 <mplakalovi1@etf.unsa.ba>
2018-11-07 00:45:57 +0100

    Uradjena metoda ChessPiece;

--- src/ba/unsa/etf/rpr/ChessPiece.java
+++ src/ba/unsa/etf/rpr/ChessPiece.java
@@ -5,29 +5,35 @@ public abstract class ChessPiece { //apstraktna klasa Sahovskih figura;
     private String position;
     private Color color;
 
-    public static enum Color{ //pobrojani tip Color ; // static je suvisno,ovdje jer se podrazumjeva ;
-        BLACK,WHITE;
+    public static enum Color { //pobrojani tip Color ; // static je suvisno,ovdje jer se podrazumjeva ;
+        BLACK, WHITE;
     }
 
-    public ChessPiece(String position,Color color){ //konstruktor
-        //provjere odgovarajuce
-        this.position=position;
-        this.color=color;
+    public ChessPiece(String position, Color color) { //konstruktor
+        check(position);//odg provjera;
+        this.position = position;
+        this.color = color;
     }
-    public String getPosition(){ //getter za positon
+
+    public String getPosition() { //getter za positon
         return position;
     }
-    public Color getColor(){ //getter za boju
+
+    public Color getColor() { //getter za boju
         return color;
     }
-    public void move(String position){
-        //provjere odgovarajuce
-        this.position=position;
+
+    public void move(String position) {
+        check(position);//provjere ,ako treba bacit ce izuzetak;
+        this.position = position;
     }
-    public static void check(String position){ //pomocna f-ja za provjeru ispravnosti formata i pozicije figure;
-        position=position.toUpperCase();//prebacujemo u velika slova sve zbog manjeg posla;
-        if( position.charAt(0)>'H' || position.charAt(0)<'A' || position.charAt(1)>'8' || position.charAt(1)<'1'){
+
+    public static void check(String position) { //pomocna f-ja za provjeru ispravnosti formata i pozicije figure;
+        position = position.toUpperCase();  //prebacujemo u velika slova sve zbog manjeg posla;
+
+        if (position.charAt(0) > 'H' || position.charAt(0) < 'A' || position.charAt(1) > '8' || position.charAt(1) < '1' || position.length() != 2) {
             throw new IllegalArgumentException(); //provjeravamo da li je pozicija validna,ako nije bacamo izuzetak;
         }
     }
+
 }
