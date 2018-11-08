d7105397a4e3ee0fbd76897e3cd50db535eaec0e
mplakalovi1 <mplakalovi1@etf.unsa.ba>
2018-11-08 15:59:39 +0100

    Class Bishop;constr. ,mathod move & metod checkQueen;

--- src/ba/unsa/etf/rpr/Bishop.java
+++ src/ba/unsa/etf/rpr/Bishop.java
@@ -1,22 +1,32 @@
 package ba.unsa.etf.rpr;
 
 public class Bishop extends ChessPiece {
-    public Bishop(String position,Color color){
-        super(position,color);
+    public Bishop(String position, Color color) {
+        super(position, color);
     }
+
     @Override
-    public void move(String position){
+    public void move(String position) {
         this.checkBishop(position);
         super.move(position);
         super.setPosition(position);
     }
-    public void checkBishop(String position){
-        position=position.toUpperCase();//sve velika zbog tretmana za ista mala,i velika slova;
-        String pocetna=super.getPosition().toUpperCase(); //pocetna pozicija figure,u ovom slucaju lovca;
-        //najprije gledamo da li se krece dijagonalno udesno;
-        if(pocetna.charAt(0)-pocetna.charAt(1)!=position.charAt(0)-position.charAt(1)){ //ako se ne krecemo dijagonalno udesno,provjeravamo da li se krecemo dijagonalno u lijevo;
-                if(pocetna.charAt(0)!=position.charAt(0)-1 && )
+
+    public void checkBishop(String position) {
+        position = position.toUpperCase();//sve velika zbog tretmana za ista mala,i velika slova;
+        String pocetna = super.getPosition().toUpperCase(); //pocetna pozicija figure,u ovom slucaju lovca;
+        boolean validno = false; //postavljamo na false prije provjere;
+        //da li se lovac krece dijagonalno;
+        for (int i = -7; i < 8; i++) {
+            if (i != 0) {
+                if (pocetna.charAt(0) + i == position.charAt(0) && pocetna.charAt(1) + i == position.charAt(1)) {
+                    validno = true;
+                    break;
+                }
             }
+        }
+        if (!validno) {
+            throw new IllegalChessMoveException();
         }
 
     }
