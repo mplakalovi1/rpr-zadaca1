1d111caf55ed08f6d177b0be31646dcd339db815
mplakalovi1 <mplakalovi1@etf.unsa.ba>
2018-11-08 22:19:31 +0100

    klasa King radjena, konstruktor,metoda move i checkKing

---
+++ src/ba/unsa/etf/rpr/King.java
@@ -0,0 +1,34 @@
+package ba.unsa.etf.rpr;
+
+import java.util.HashSet;
+
+public class King extends ChessPiece {
+    public King(String position, Color color) {
+        super(position, color);
+    }
+
+    @Override
+    public void move(String position) {
+        super.move(position);
+        this.checkKing(position);
+        super.setPosition(position);
+    }
+
+    public void checkKing(String position) throws IllegalChessMoveException {
+        position = position.toUpperCase();
+        String pocetna = super.getPosition().toUpperCase();
+        //Kralj se može pokretati svim smjerovima za jedno polje;
+        HashSet<String> evidencija = new HashSet<>();
+        for (int i = -1; i < 2; i++) { //sve moguce kretnje evidentiramo
+            if (i != 0) {
+                evidencija.add((char) (pocetna.charAt(0) + i) + "" + pocetna.charAt(1));
+                evidencija.add(pocetna.charAt(0) + "" + (char) (pocetna.charAt(1) + i));
+                evidencija.add((char) (pocetna.charAt(0) + i) + "" + (char) (pocetna.charAt(1) + i));
+                evidencija.add((char) (pocetna.charAt(0) + i) + "" + (char) (pocetna.charAt(1) - i));
+            }
+        }
+        if (!evidencija.contains(position)) {
+            throw new IllegalChessMoveException();
+        }
+    }
+}
