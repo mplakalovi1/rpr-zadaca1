4ddc810e0e398e9fe63d3f122d7221deeecedea5
mplakalovi1 <mplakalovi1@etf.unsa.ba>
2018-11-08 16:09:39 +0100

    Class Queen;constr. ,mathod move & metod checkQueen; prepravka...

--- src/ba/unsa/etf/rpr/Queen.java
+++ src/ba/unsa/etf/rpr/Queen.java
@@ -16,8 +16,20 @@ public class Queen extends ChessPiece {
         position = position.toUpperCase();
         String pocetna = super.getPosition().toUpperCase();//prebacujemo pocetnu poziciju i zadanu u vrlika slova;
 
-        if (position.charAt(0) != pocetna.charAt(0) && position.charAt(1) != pocetna.charAt(1) && (pocetna.charAt(0) - pocetna.charAt(1) != position.charAt(0) - position.charAt(1))) {
+        boolean validno = false; //postavljamo na false prije provjere;
+        //da li se Queen krece dijagonalno;
+        for (int i = -7; i < 8; i++) {
+            if (i != 0) {
+                if (pocetna.charAt(0) + i == position.charAt(0) && pocetna.charAt(1) + i == position.charAt(1)) {
+                    validno = true;
+                    break;
+                }
+            }
+        }
+
+        if (position.charAt(0) != pocetna.charAt(0) && position.charAt(1) != pocetna.charAt(1) && !validno) {
             throw new IllegalChessMoveException();
         }
+
     }
 }
